41a7db7ac3f75820cae0ded39f2bdd7980e5313f
Daniel Huaman <danielhuaman@outlook.com>
2021-11-08 14:10:27 -0500

    feat: design maintenance page Products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './accesss/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/maintenances/products/products.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'maintenance/products', component: ProductsComponent },
 ];
 
 @NgModule({
