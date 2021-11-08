233fea938425356aa1339318336d28722b8cd718
Daniel Huaman <danielhuaman@outlook.com>
2021-11-08 16:39:39 -0500

    feat: new page NotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CampaignsComponent } from './pages/maintenances/campaigns/campaigns.component';
 import { MaintenancesComponent } from './pages/maintenances/maintenances.component';
 import { ProductsComponent } from './pages/maintenances/products/products.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,6 +14,8 @@ const routes: Routes = [
   { path: 'maintenances', component: MaintenancesComponent },
   { path: 'maintenances/products', component: ProductsComponent },
   { path: 'maintenances/campaigns', component: CampaignsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', pathMatch: 'full', component: NotFoundComponent },
 ];
 
 @NgModule({
