22e2c8186071a4195716043dac8c839b0d8b7928
Daniel Huaman <danielhuaman@outlook.com>
2021-11-08 16:25:04 -0500

    feat: configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './accesss/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CampaignsComponent } from './pages/maintenances/campaigns/campaigns.component';
+import { MaintenancesComponent } from './pages/maintenances/maintenances.component';
 import { ProductsComponent } from './pages/maintenances/products/products.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'maintenance/products', component: ProductsComponent },
-  { path: 'maintenance/campaigns', component: CampaignsComponent },
+  { path: 'maintenances', component: MaintenancesComponent },
+  { path: 'maintenances/products', component: ProductsComponent },
+  { path: 'maintenances/campaigns', component: CampaignsComponent },
 ];
 
 @NgModule({
