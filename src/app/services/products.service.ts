d9e2372cd72c5baf1f5c4155d2da8300291fbb58
Daniel Huaman <daniel.huaman@b89.io>
2021-12-05 21:48:22 -0500

    feat: update product services

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,26 +2,43 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IProduct } from '../interfaces/product.interface';
+import { environment } from 'src/environments/environment';
+import { IResponse } from '../interfaces/response.interface';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
 
-  headers: HttpHeaders = new HttpHeaders().set('Authorization', 'Bearer fake-jwt-token');
+  headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('JWT-TOKEN')}`);
+  path: string = 'api/product/'
   constructor(private http: HttpClient) { }
 
-  listUsers(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('/products', { headers: this.headers }).pipe();
+  list(): Observable<IResponse> {
+    return this.http.get<IResponse>(`${environment.apiUrl + this.path}getallproducts`, { headers: this.headers }).pipe();
   }
 
-  getUser(idProduct: string): Observable<IProduct> {
-    return this.http.get<IProduct>(`/products/${idProduct}`, { headers: this.headers }).pipe();
+  get(idProduct: string): Observable<IProduct> {
+    return this.http.get<IProduct>(`${environment.apiUrl + this.path}getproduct?idProduct=${idProduct}`, { headers: this.headers }).pipe();
   }
 
-  newUser(product: IProduct): Observable<IProduct> {
-    return this.http.post<IProduct>(`/products/register`, product).pipe();
+  add(product: IProduct): Observable<IProduct> {
+    const request = {
+      nombreProducto: product.nombreProducto,
+      descProducto: product.descProducto,
+      precioProducto: Number(product.precioProducto),
+      idCategoriaProducto: Number(product.idCategoriaProducto),
+    }
+
+    return this.http.post<IProduct>(`${environment.apiUrl + this.path}insertproduct`, request).pipe();
   }
 
-  updateUser(product: IProduct): Observable<IProduct> {
-    return this.http.put<IProduct>(`/products/update`, product).pipe();
+  update(product: IProduct): Observable<IProduct> {
+    const request = {
+      idProducto: product.idProducto,
+      nombreProducto: product.nombreProducto,
+      descProducto: product.descProducto,
+      precioProducto: Number(product.precioProducto),
+      idCategoriaProducto: Number(product.idCategoriaProducto),
+    }
+    return this.http.post<IProduct>(`${environment.apiUrl + this.path}updateproduct`, request).pipe();
   }
 }
